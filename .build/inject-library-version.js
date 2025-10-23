f03fccf60587694fa182777f2d006c04aa18f540
neunerlei <ich@neunerlei.eu>
2025-10-23 07:27:41 +0200

    fix: ensure dynamic version replacement

--- .build/inject-library-version.js
+++ .build/inject-library-version.js
@@ -3,10 +3,10 @@ import fs from 'fs';
 const versionFilePath = './src/version.ts';
 
 export function preCommit({version}) {
-    const defaultVersion = 'version: \'0.0.0\'';
+    const versionPattern = /version:\s*'(\d+\.\d+\.\d+)'/;
     const newVersion = `version: '${version}'`;
     let content = fs.readFileSync(versionFilePath, 'utf8');
-    content = content.replace(defaultVersion, newVersion);
+    content = content.replace(versionPattern, newVersion);
     fs.writeFileSync(versionFilePath, content, 'utf8');
     console.log(`Injected version ${version} into ${versionFilePath}`);
     console.debug('Content after injection:', content);
