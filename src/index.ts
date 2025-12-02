2f4022cfc370a7dcdd4c8db0661b6339b9deea9f
neunerlei <ich@neunerlei.eu>
2025-12-02 07:29:08 +0100

    feat: make common types externally available

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,19 @@
  * and user authentication for HAWKI chat applications.
  */
 
-export * from './HawkiClient.js';
-export * from './internal/logger.js';
+export {createHawkiClient} from './HawkiClient.js';
+export {createDebugLogger, createSilentLogger} from './internal/logger.js';
+
+export type {Logger} from './internal/logger.js';
+export type {HawkiClientOptions} from './HawkiClient.js';
+export type {HawkiClient} from './HawkiClient.js';
+export type {User} from './internal/users/users.js';
+export type {Locale} from './internal/translation/translation.js';
+export type {Room, CreateRoomArgs, UpdateRoomArgs} from './internal/rooms/rooms.js';
+export type {RoomMessage, RoomMessageAiInfo, RoomMessageAttachment} from './internal/rooms/messages/messages.js';
+export type {Member, MemberRole, UserMemberRole} from './internal/rooms/members/members.js';
+export type {ReactiveStore, ReactiveStoreSubscriber} from './internal/resources/stores/stores.js';
+export type {ReactiveStoreFront, ReactiveStoreFrontProvider} from './internal/resources/stores/ReactiveStoreFront.js';
+export type {ProfileUpdateBody} from './internal/profile/profile.js';
+export type {StorageFile, AvatarLimits, AttachmentLimits} from './internal/files/files.js';
+export type {DefaultModelType, DefaultModelTypeOrAlias, SystemModelType, SystemPromptType} from './internal/ai/ai.js';
